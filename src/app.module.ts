520926cbb349aee67b8b5843e3b7af8a5de6d053
Kishu Maharjan <mrhznkishu.10@gmail.com>
2023-08-08 10:59:29 +0545

    fix: add typeormmodule dependency in task modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import entities from './typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
